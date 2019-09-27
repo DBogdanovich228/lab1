52f3e3468863d639decaa7fc0aa8a8d8ec3cc3c8
DBogdanovich228 <55573297+DBogdanovich228@users.noreply.github.com>
2019-09-27 09:52:55 +0300

    Add files via upload

---
+++ Lab1.1.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x, res;
+    scanf("%d",&x);
+    res=8*x*x*x;
+    printf("res=%d",res);
+
+    return 0;
+}
---
+++ lab1.2.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+float x, y, z, s, a, b, c;
+scanf("%d"&x,y,z);
+a=sqrt(pow));
+
+
+
+    return 0;
+}
