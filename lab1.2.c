72bc8df8f10a7cda21291753103ee997ba32afb4
DBogdanovich228 <55573297+DBogdanovich228@users.noreply.github.com>
2019-10-17 18:25:46 +0300

    Add files via upload

---
+++ lab1.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+
+
+void main(void) {setlocale(LC_ALL,"Russian");
+float x,y,z,a,c,d,s;
+printf("введите x,y,z\n");
+scanf("%f %f %f",&x,&y,&z);
+a=cbrt(x)+pow(x,y+2);
+c=sqrt(10*a);
+d=pow(asin(z),2)-fabs(x-y);
+s=c*d;
+printf("res=%f", s);
+	return 0;
+}
